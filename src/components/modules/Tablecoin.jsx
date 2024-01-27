6b64267c04d3809ea653f220f5b3a039627c0752
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-01-27 14:56:20 +0330

    table row added

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -1,8 +1,44 @@
+import chartUp from '../../assets/chart-up.svg';
+import chartDown from '../../assets/chart-down.svg';
+
 //eslint-disable-next-line
 function TableCoin({ coins }) {
-    console.log(coins);
     return (
-        <div>TableCoin</div>
+        <div>
+            <table>
+                <thead>
+                    <tr><th>Coin</th></tr>
+                    <tr><th>Name</th></tr>
+                    <tr><th>Price</th></tr>
+                    <tr><th>24h</th></tr>
+                    <tr><th>Total Volume</th></tr>
+                    <tr><th></th></tr>
+                </thead>
+                <tbody>
+                    {/* eslint-disable-next-line */}
+                    {coins.map(coin =>
+                        <tr key={coin.id}>
+                            <td>
+                                <div>
+                                    <img src={coin.image} alt="" />
+                                    <span>{coin.symbol.toUpperCase()}</span>
+                                </div>
+                            </td>
+                            <td>{coin.name}</td>
+                            <td>${coin.current_price.toLocaleString()}</td>
+                            <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+                            <td>{coin.total_volume.toLocaleString()}</td>
+                            <td>
+                                <img
+                                    src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown}
+                                    alt={coin.name}
+                                />
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
