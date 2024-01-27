a61239261d7a2697008087d892c9c08e5ba177c0
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-01-27 15:12:34 +0330

    loader added

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -1,43 +1,47 @@
+import { RotatingLines } from 'react-loader-spinner';
 import chartUp from '../../assets/chart-up.svg';
 import chartDown from '../../assets/chart-down.svg';
 
 //eslint-disable-next-line
-function TableCoin({ coins }) {
+function TableCoin({ coins, isLoading }) {
     return (
         <div>
-            <table>
-                <thead>
-                    <tr><th>Coin</th></tr>
-                    <tr><th>Name</th></tr>
-                    <tr><th>Price</th></tr>
-                    <tr><th>24h</th></tr>
-                    <tr><th>Total Volume</th></tr>
-                    <tr><th></th></tr>
-                </thead>
-                <tbody>
-                    {/* eslint-disable-next-line */}
-                    {coins.map(coin =>
-                        <tr key={coin.id}>
-                            <td>
-                                <div>
-                                    <img src={coin.image} alt="" />
-                                    <span>{coin.symbol.toUpperCase()}</span>
-                                </div>
-                            </td>
-                            <td>{coin.name}</td>
-                            <td>${coin.current_price.toLocaleString()}</td>
-                            <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                            <td>{coin.total_volume.toLocaleString()}</td>
-                            <td>
-                                <img
-                                    src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown}
-                                    alt={coin.name}
-                                />
-                            </td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+            {isLoading ? <RotatingLines strokeColor='#3874ff' strokeWidth='2' /> : (
+                <table>
+                    <thead>
+                        <tr><th>Coin</th></tr>
+                        <tr><th>Name</th></tr>
+                        <tr><th>Price</th></tr>
+                        <tr><th>24h</th></tr>
+                        <tr><th>Total Volume</th></tr>
+                        <tr><th></th></tr>
+                    </thead>
+                    <tbody>
+                        {/* eslint-disable-next-line */}
+                        {coins.map(coin =>
+                            <tr key={coin.id}>
+                                <td>
+                                    <div>
+                                        <img src={coin.image} alt="" />
+                                        <span>{coin.symbol.toUpperCase()}</span>
+                                    </div>
+                                </td>
+                                <td>{coin.name}</td>
+                                <td>${coin.current_price.toLocaleString()}</td>
+                                <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+                                <td>{coin.total_volume.toLocaleString()}</td>
+                                <td>
+                                    <img
+                                        src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown}
+                                        alt={coin.name}
+                                    />
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            )
+            }
         </div>
     )
 }
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -3,16 +3,18 @@ import Tablecoin from '../modules/Tablecoin'
 import { getCoinList } from "../../services/cryptoApi"
 function HomePage() {
     const [coins, setCoins] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
         const getData = async () => {
             const res = await fetch(getCoinList())
             const json = await res.json();
-            setCoins(json)
+            setCoins(json);
+            setIsLoading(false);
         }
         getData()
     }, [])
     return (
-        <div><Tablecoin coins={coins} /></div>
+        <div><Tablecoin coins={coins} isLoading={isLoading} /></div>
     )
 }
 
