0084270b7abc3cf4bc3bafa723eefcfad1d4eeb3
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-01-27 23:45:07 +0330

    search modal added

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,22 +2,29 @@ import { useEffect, useState } from "react"
 import Tablecoin from '../modules/Tablecoin'
 import { getCoinList } from "../../services/cryptoApi"
 import Pagination from "../modules/Pagination"
+import Search from "../modules/Search"
 function HomePage() {
     const [coins, setCoins] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [page, setPage] = useState(1);
+    const [currency, setCurrency] = useState("usd")
     useEffect(() => {
         setIsLoading(true)
         const getData = async () => {
-            const res = await fetch(getCoinList(page))
-            const json = await res.json();
-            setCoins(json);
-            setIsLoading(false);
+            try {
+                const res = await fetch(getCoinList(page, currency))
+                const json = await res.json();
+                setCoins(json);
+                setIsLoading(false);
+            } catch (error) {
+                console.log(error);
+            }
         }
         getData()
-    }, [page])
+    }, [page, currency])
     return (
         <div>
+            <Search currency={currency} setCurrency={setCurrency} />
             <Tablecoin coins={coins} isLoading={isLoading} />
             <Pagination page={page} setPage={setPage} />
         </div>
