7a340e762e604aaf373531b9241585edc085b32d
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-01-27 15:28:24 +0330

    styles added

--- src/components/modules/Tablecoin.jsx
+++ src/components/modules/Tablecoin.jsx
@@ -1,34 +1,40 @@
 import { RotatingLines } from 'react-loader-spinner';
 import chartUp from '../../assets/chart-up.svg';
 import chartDown from '../../assets/chart-down.svg';
+import styles from './Tablecoin.module.css';
 
 //eslint-disable-next-line
 function TableCoin({ coins, isLoading }) {
     return (
-        <div>
+        <div className={styles.container}>
             {isLoading ? <RotatingLines strokeColor='#3874ff' strokeWidth='2' /> : (
-                <table>
+                <table className={styles.table}>
                     <thead>
-                        <tr><th>Coin</th></tr>
-                        <tr><th>Name</th></tr>
-                        <tr><th>Price</th></tr>
-                        <tr><th>24h</th></tr>
-                        <tr><th>Total Volume</th></tr>
-                        <tr><th></th></tr>
+                        <tr>
+                            <th>Coin</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>24h</th>
+                            <th>Total Volume</th>
+                            <th></th>
+                        </tr>
                     </thead>
                     <tbody>
                         {/* eslint-disable-next-line */}
                         {coins.map(coin =>
                             <tr key={coin.id}>
                                 <td>
-                                    <div>
+                                    <div className={styles.symbol}>
                                         <img src={coin.image} alt="" />
                                         <span>{coin.symbol.toUpperCase()}</span>
                                     </div>
                                 </td>
                                 <td>{coin.name}</td>
                                 <td>${coin.current_price.toLocaleString()}</td>
-                                <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+                                <td
+                                    className={coin.price_change_percentage_24h > 0 ? styles.success : styles.error}>
+                                    {coin.price_change_percentage_24h.toFixed(2)}%
+                                </td>
                                 <td>{coin.total_volume.toLocaleString()}</td>
                                 <td>
                                     <img
